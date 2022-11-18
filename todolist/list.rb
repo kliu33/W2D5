d14e8beb1b7e8be8a6cf002a93adaf57524f3eb0
kliu33 <kliu33@buffalo.edu>
2022-11-18 15:12:55 -0500

    more functional list

--- todolist/list.rb
+++ todolist/list.rb
@@ -20,7 +20,10 @@ class List
     end
 
     def valid_index?(index)
-        return false if index < 0 || index > size - 1
+        if index < 0 || index > size - 1
+            p "invalid index #{index}"
+            return false 
+        end
         true
     end 
 
@@ -38,4 +41,59 @@ class List
     def priority
         @items[0]
     end
+
+    def print
+        puts "Index | Item           | Deadline"
+        @items.each_with_index do |item, idx|
+            puts "#{idx}     | #{item.title.ljust(12)}   | #{item.deadline}"
+        end
+        puts "\n"
+    end
+
+    def print_item_full(index)
+        if valid_index?(index)
+            puts "----------------------------------------"
+            puts "#{@items[index].title.ljust(25)} #{@items[index].deadline}"
+            puts @items[index].description
+            puts "----------------------------------------"
+        end
+    end
+
+    def print_priority
+        print_item_full(0)
+    end
+
+    def up(index, amount = 1)
+        return false if !valid_index?(index)
+        amount.times do 
+            if index != 0
+                swap(index, index-1)
+                index -= 1
+            end
+        end
+        true
+    end
+
+    def down(index, amount = 1)
+        return false if !valid_index?(index)
+        amount.times do 
+            if index != size - 1
+                swap(index, index+1)
+                index += 1
+            end
+        end
+        true
+    end
 end
+
+#my_list = List.new('Groceries')
+#my_list.add_item('cheese', '2019-10-25', 'Get American and Feta for good measure.')
+#my_list.add_item('toothpaste', '2019-10-25')
+#my_list.add_item('shampoo', '2019-10-24')
+#my_list.add_item('candy', '2019-10-31', '4 bags should be enough')
+#my_list.print
+#my_list.print_priority
+#my_list.up(3,5)
+#my_list.print
+#my_list.down(-1,2)
+#my_list.print
