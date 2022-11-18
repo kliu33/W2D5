a3afdeef6a903f03f85f6460be9b94ef52387584
kliu33 <kliu33@buffalo.edu>
2022-11-18 14:53:29 -0500

    functional list

--- todolist/list.rb
+++ todolist/list.rb
@@ -1,1 +1,41 @@
+require_relative "item.rb"
 
+class List
+
+    attr_accessor :label
+
+    def initialize(label)
+        @label = label
+        @items = []
+    end
+
+    def add_item(title, deadline, description="")
+        return false if !Item.valid_date?(deadline)
+        @items << Item.new(title, deadline, description)
+        true
+    end
+
+    def size
+        @items.length
+    end
+
+    def valid_index?(index)
+        return false if index < 0 || index > size - 1
+        true
+    end 
+
+    def swap(index_1, index_2)
+        return false if !valid_index?(index_1) || !valid_index?(index_2)
+        @items[index_1] , @items[index_2] = @items[index_2] , @items[index_1]
+        true
+    end
+
+    def [](index)
+        return nil if !valid_index?(index)
+        @items[index]
+    end
+
+    def priority
+        @items[0]
+    end
+end
