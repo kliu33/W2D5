4e7326c5f9abc33dd46828bc25f1dcfae1150d85
kliu33 <kliu33@buffalo.edu>
2022-11-18 14:33:59 -0500

    finish item

---
+++ todolist/item.rb
@@ -0,0 +1,37 @@
+class Item
+
+    attr_reader :deadline
+    attr_accessor :title, :description
+
+    def self.valid_date?(date)
+        parts = date.split("-")
+        return false if parts[0].length != 4
+        return false if parts[1].length != 2
+        return false if parts[2].length != 2
+
+        digs = "1234567890"
+        parts.each do |part|
+            part.each_char do |ele|
+                return false if !digs.include?(ele)
+            end
+        end
+
+        return false if parts[1].to_i > 12 || parts[1].to_i < 1
+        return false if parts[2].to_i > 31 || parts[1].to_i < 1
+
+        true
+    end
+
+    def initialize(title, deadline, description)
+        @title = title
+        @description = description
+        
+        raise "deadline is not valid" unless self.class.valid_date?(deadline)
+        @deadline = deadline
+    end
+
+    def deadline=(deadline)
+        raise "deadline is not valid" unless self.class.valid_date?(deadline)
+        @deadline = deadline
+    end 
+end 
